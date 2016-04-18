0525915f33ae09ff30a7bfc0a4fcf9fd47cd0512
Foogy1 <jdbeaver@usc.edu>
2016-04-17 21:50:35 -0700

    Added files via upload

---
+++ PhasePrototype.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhasePrototype : MonoBehaviour {
+
+	int count = 0;
+	public bool buildPhase = true;
+
+	public Color phaseColor = Color.green /*= red;*/;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		count++;
+
+		if (count >= 300) {
+
+			buildPhase = false;
+			phaseColor = Color.red;
+			this.GetComponent<SpriteRenderer> ().color = phaseColor;
+
+		}
+
+
+
+
+	}
+}
